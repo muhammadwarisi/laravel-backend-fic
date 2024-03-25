1173f4f40fdf786870706e98d7aeb4664d899963
muhammadwarisi <mw.a.seven.one@gmail.com>
2024-03-25 07:01:28 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\DoctorSchedule;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            "email"=> 'ari@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('ari1234'),
+            'phone'=> '123456789',
+        ]);
+
+        // seeder profile_clinic manual
+        \App\Models\ProfileClinic::factory()->create([
+            'name' => 'Ari Clinic',
+            'address' => 'Jl. Ahmad Wongso',
+            'phone' => '085787405635',
+            'email' => 'dr.ari@gmail.com',
+            'doctor_name' => 'Dr. Ari',
+            'unique_code' => '123456',
+        ]);
+
+        // call
+        $this->call([
+            DoctorSeeder::class,
+            DoctorSchedule::class,
+        ]);
+    }
+}
---
+++ database/seeders/DoctorScheduleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DoctorScheduleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //create Doctor Schedule
+        \App\Models\Doctor::create([
+            'doctor_id' => 1,
+            'day' => 'monday',
+            'time' => '08:00 - 12:00',
+        ]);
+
+        // Auto Generate Doctor Schedule
+        \App\Models\Doctor::all()->each(function ($doctor){
+                \App\Models\DoctorSchedule::factory()->count(10)->create([
+                    'doctor_id' => $doctor->id
+                ]);
+        });
+    }
+}
